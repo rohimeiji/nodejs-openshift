c75282adb69b59f4f3246e32db02d8dfe458a16e
rochimeiji <rochim@lingkar9.com>
2017-10-03 17:12:25 +0700

    Update

--- server.js
+++ server.js
@@ -26,8 +26,10 @@ if(!host){
 	process.env.spurl = "https://sohibpulsa.com";
 	process.env.cachename = "rochimeiji";
 }
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8000);
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+app.set('port', (process.env.PORT || 5000));
+// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8000);
+// app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // Listen Port
 server.listen(app.get('port'), app.get('ipaddr'));
